0d48deb85dedbbe1db6b3361d6172d5f62738aa8
Dadibom <dadibom@gmail.com>
2024-10-14 16:12:28 +0200

    add gameboy emulation layer

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -5,6 +5,20 @@
 
 #pragma once
 
+#define MIRYOKU_LAYER_LIST \
+MIRYOKU_X(BASE,   "Base") \
+MIRYOKU_X(EXTRA,  "Extra") \
+MIRYOKU_X(TAP,    "Tap") \
+MIRYOKU_X(BUTTON, "Button") \
+MIRYOKU_X(NAV,    "Nav") \
+MIRYOKU_X(MOUSE,  "Mouse") \
+MIRYOKU_X(MEDIA,  "Media") \
+MIRYOKU_X(NUM,    "Num") \
+MIRYOKU_X(SYM,    "Sym") \
+MIRYOKU_X(FUN,    "Fun") \
+MIRYOKU_X(GB,     "GB")
+
+
 #define MIRYOKU_LAYERMAPPING_TAP(\
 K00,   K01,   K02,   K03,   K04,                 K05,   K06,   K07,   K08,   K09,\
 K10,   K11,   K12,   K13,   K14,                 K15,   K16,   K17,   K18,   K19,\
@@ -26,9 +40,24 @@ K20,   K21,   K22,   K23,   K24,          K25,   K26,   K27,   K28,   K29,\
 N30,   N31,   K32,   K33,   K34,          K35,   K36,   K37,   N38,   N39\
 )\
 LAYOUT_ortho_5x10(\
-KC_NO, KC_NO, KC_NO, KC_NO, DF(U_TAP),    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+KC_NO, KC_NO, KC_NO, DF(U_TAP), DF(U_TAP),    DF(U_GB), DF(U_GB), KC_NO, KC_NO, KC_NO, \
 K00,   K01,   K02,   K03,   K04,          K05,   K06,   K07,   K08,   K09,\
 K10,   K11,   K12,   K13,   K14,          K15,   K16,   K17,   K18,   K19,\
 K20,   K21,   K22,   K23,   K24,          K25,   K26,   K27,   K28,   K29,\
 LCTL(KC_C),LCTL(KC_V),K32,K33,K34,        K35,   K36,   K37,   KC_NO, KC_NO\
+)
+
+// gb emulator layout
+#define MIRYOKU_LAYERMAPPING_GB\
+K00,   K01,   K02,   K03,   K04,                 K05,   K06,   K07,   K08,   K09,\
+K10,   K11,   K12,   K13,   K14,                 K15,   K16,   K17,   K18,   K19,\
+K20,   K21,   K22,   K23,   K24,                 K25,   K26,   K27,   K28,   K29,\
+N30,   N31,   K32,   K33,   K34,                 K35,   K36,   K37,   N38,   N39\
+)\
+LAYOUT_ortho_5x10(\
+DF(U_BASE),              DF(U_BASE),              DF(U_BASE),              DF(U_BASE),              DF(U_BASE),         DF(U_BASE),              DF(U_BASE),              DF(U_BASE),              DF(U_BASE),              DF(U_BASE),\
+DF(U_BASE),              DF(U_BASE),              DF(U_BASE),              DF(U_BASE),              DF(U_BASE),         DF(U_BASE),              DF(U_BASE),              DF(U_BASE),              DF(U_BASE),              DF(U_BASE),\
+KC_Z,                    KC_X,                    KC_A,                    KC_S,                    DF(U_BASE),         DF(U_BASE),              KC_LEFT,                 KC_DOWN,                 KC_UP,                   KC_RIGHT,\
+DF(U_BASE),              DF(U_BASE),              DF(U_BASE),              DF(U_BASE),              DF(U_BASE),         DF(U_BASE),              DF(U_BASE),              DF(U_BASE),              DF(U_BASE),              DF(U_BASE),\
+DF(U_BASE),              DF(U_BASE),              DF(U_BASE),              DF(U_BASE),              DF(U_BASE),         DF(U_BASE),              DF(U_BASE),              DF(U_BASE),              DF(U_BASE),              DF(U_BASE),\
 )
