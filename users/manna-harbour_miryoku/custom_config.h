bd30df98fe791227a30a3e56f9545660c35b847a
Dadibom <dadibom@gmail.com>
2024-10-14 19:04:13 +0200

    add layer def

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -5,6 +5,27 @@
 
 #pragma once
 
+#define MIRYOKU_LAYER_LIST \
+MIRYOKU_X(BASE,   "Base") \
+MIRYOKU_X(EXTRA,  "Extra") \
+MIRYOKU_X(TAP,    "Tap") \
+MIRYOKU_X(BUTTON, "Button") \
+MIRYOKU_X(NAV,    "Nav") \
+MIRYOKU_X(MOUSE,  "Mouse") \
+MIRYOKU_X(MEDIA,  "Media") \
+MIRYOKU_X(NUM,    "Num") \
+MIRYOKU_X(SYM,    "Sym") \
+MIRYOKU_X(FUN,    "Fun") \
+MIRYOKU_X(GBE,     "Gameboy")
+
+// i think i need the following, it's just junk as i'll remap it later anyway.
+#define MIRYOKU_LAYER_GBE \
+KC_Q,              KC_W,              KC_E,              KC_R,              KC_T,              KC_Y,              KC_U,              KC_I,              KC_O,              KC_P,              \
+KC_A,              KC_S,              KC_D,              KC_F,              KC_G,              KC_H,              KC_J,              KC_K,              KC_L,              KC_QUOT,           \
+KC_Z,              KC_X,              KC_C,              KC_V,              KC_B,              KC_N,              KC_M,              KC_COMM,           KC_DOT,            KC_SLSH,           \
+U_NP,              U_NP,              KC_ESC,            KC_SPC,            KC_TAB,            KC_ENT,            KC_BSPC,           KC_DEL,            U_NP,              U_NP
+
+
 #define MIRYOKU_LAYERMAPPING_TAP(\
 K00,   K01,   K02,   K03,   K04,                 K05,   K06,   K07,   K08,   K09,\
 K10,   K11,   K12,   K13,   K14,                 K15,   K16,   K17,   K18,   K19,\
@@ -46,16 +67,3 @@ KC_Z,                    KC_X,                    KC_A,                    KC_S,
 DF(U_BASE),              DF(U_BASE),              DF(U_BASE),              DF(U_BASE),              DF(U_BASE),         DF(U_BASE),              DF(U_BASE),              DF(U_BASE),              DF(U_BASE),              DF(U_BASE),\
 DF(U_BASE),              DF(U_BASE),              DF(U_BASE),              DF(U_BASE),              DF(U_BASE),         DF(U_BASE),              DF(U_BASE),              DF(U_BASE),              DF(U_BASE),              KC_MEDIA_PLAY_PAUSE\
 )
-
-#define MIRYOKU_LAYER_LIST \
-MIRYOKU_X(BASE,   "Base") \
-MIRYOKU_X(EXTRA,  "Extra") \
-MIRYOKU_X(TAP,    "Tap") \
-MIRYOKU_X(BUTTON, "Button") \
-MIRYOKU_X(NAV,    "Nav") \
-MIRYOKU_X(MOUSE,  "Mouse") \
-MIRYOKU_X(MEDIA,  "Media") \
-MIRYOKU_X(NUM,    "Num") \
-MIRYOKU_X(SYM,    "Sym") \
-MIRYOKU_X(FUN,    "Fun") \
-MIRYOKU_X(GBE,     "Gameboy")
