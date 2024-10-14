e9ef0ae30c7d4e8837ea0406cd9aca91c5b26066
Dadibom <dadibom@gmail.com>
2024-10-14 16:41:47 +0200

    remove miryoku_layermapping again

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -18,8 +18,6 @@ MIRYOKU_X(SYM,    "Sym") \
 MIRYOKU_X(FUN,    "Fun") \
 MIRYOKU_X(GB,     "GB")
 
-#define MIRYOKU_LAYERMAPPING_GB MIRYOKU_MAPPING
-
 #define MIRYOKU_LAYERMAPPING_TAP(\
 K00,   K01,   K02,   K03,   K04,                 K05,   K06,   K07,   K08,   K09,\
 K10,   K11,   K12,   K13,   K14,                 K15,   K16,   K17,   K18,   K19,\
@@ -41,7 +39,7 @@ K20,   K21,   K22,   K23,   K24,          K25,   K26,   K27,   K28,   K29,\
 N30,   N31,   K32,   K33,   K34,          K35,   K36,   K37,   N38,   N39\
 )\
 LAYOUT_ortho_5x10(\
-KC_NO, KC_NO, KC_NO, DF(U_TAP), DF(U_TAP),    DF(U_GB), DF(U_GB), KC_NO, KC_NO, KC_NO, \
+KC_NO, KC_NO, KC_NO, DF(U_TAP), DF(U_TAP),    DF(U_GB), DF(U_GB), KC_NO, KC_NO, KC_NO,\
 K00,   K01,   K02,   K03,   K04,          K05,   K06,   K07,   K08,   K09,\
 K10,   K11,   K12,   K13,   K14,          K15,   K16,   K17,   K18,   K19,\
 K20,   K21,   K22,   K23,   K24,          K25,   K26,   K27,   K28,   K29,\
